69784866bb9a4a78e5b0fb7dafbd23ed2b7284c5
ERIC.HH.LEE 李後熙 <ERIC.HH.LEE@deltaww.com>
2024-07-09 14:14:38 +0800

    feat: add to ceate vanilla project

--- src/window/default.mjs
+++ src/window/default.mjs
@@ -7,7 +7,7 @@ import Msg from '#root/util/Msg.mjs';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
-const exeFolder = 'D:\\softs\\SetDefaultBrowser';
+const exeFolder = 'D:\\softs\\SetUserFTA';
 
 const MapBrowser = new Map([
   ['CHROME', 'chrome'],
@@ -21,13 +21,17 @@ const runTask = async () => {
   Msg.notice(
     'Check: https://kolbi.cz/blog/2017/11/10/setdefaultbrowser-set-the-default-browser-per-user-on-windows-10-and-server-2016-build-1607/',
   );
-  Msg.start('point to SetDefaultBrowser folder');
+  Msg.notice('Check: https://setuserfta.com/');
+  Msg.start('point to SetUserFTA folder');
   process.chdir(exeFolder);
-  Msg.pass('point to SetDefaultBrowser folder');
-  Msg.start('change to Google Chrome');
-  await execCmd(`SetDefaultBrowser.exe ${MapBrowser.get('CHROME')}`);
-  Msg.pass('change to Google Chrome');
-  Msg.end('1. change to default Goolge Browser');
+  Msg.pass('point to SetUserFTA folder');
+  Msg.start(
+    'change to Google Chrome, but not working now cause Digital Markets Act',
+  );
+  Msg.alert('Not working now cause Digital Markets Act');
+  // await execCmd(`SetUserFTA.exe ${MapBrowser.get('CHROME')}`);
+  // Msg.pass('change to Google Chrome');
+  // Msg.end('1. change to default Goolge Browser');
 
   Msg.start('2. change Windows 10 theme');
   await execCmd(`Invoke-Expression ${__dirname}\\theme.deskthemepack`, {
